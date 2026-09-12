0b9d2a73e2ac9341e2cd278892ae94ab4d745236
fbzzvjbwpt-ui <fbzzvjbwpt@privaterelay.appleid.com>
2026-09-12 23:18:33 +0200

    Italienisch-Lernapp: veraltete ContentView auf RootTabView umleiten

--- Sarti SpritzX/ContentView.swift
+++ Sarti SpritzX/ContentView.swift
@@ -4,19 +4,14 @@
 //
 //  Created by Tobias Thiele on 12.09.26.
 //
+//  Veraltet – die App verwendet jetzt RootTabView als Einstiegspunkt.
+//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.yellow
-                .ignoresSafeArea()
-            Text("Willkommen")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundStyle(.white)
-        }
+        RootTabView()
     }
 }
 
