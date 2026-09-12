0366470062dcd257cfb0917606ab72a696a4cec0
Tobi <quadrat-riviera.7i@icloud.com>
2026-09-12 18:26:44 +0200

    Initial Commit

---
+++ Sarti SpritzX/Sarti_SpritzXApp.swift
@@ -0,0 +1,32 @@
+//
+//  Sarti_SpritzXApp.swift
+//  Sarti SpritzX
+//
+//  Created by Tobias Thiele on 12.09.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Sarti_SpritzXApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
